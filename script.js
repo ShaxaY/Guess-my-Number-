6c545784b7d1d64a3130f2bf31d8b905fd07752d
Shakhzod Yokubov <shakhzod.y@gmail.com>
2020-11-17 19:20:24 +0500

    add: implement check button function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🤦‍♂️ Enter the number!';
+  }
+});
