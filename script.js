c245056569c79048b05759b551a48d4a3ca93064
Shakhzod Yokubov <shakhzod.y@gmail.com>
2020-11-17 19:55:33 +0500

    add: implement game logic

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 'use strict';
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+const scoreUpdate = score => {
+  document.querySelector('.score').textContent = score;
+};
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '🤦‍♂️ Enter the number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      scoreUpdate(score);
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost!';
+      scoreUpdate(0);
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too Low!';
+      score--;
+      scoreUpdate(score);
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost!';
+      scoreUpdate(0);
+    }
   }
 });
