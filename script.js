8e0c4768533529cb0390eca74abdce3b5502dcf4
Shakhzod Yokubov <shakhzod.y@gmail.com>
2020-11-18 13:21:05 +0500

    add: implement again button

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 'use strict';
-
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
 const scoreUpdate = score => {
   document.querySelector('.score').textContent = score;
 };
+
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+});
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
@@ -16,8 +27,9 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.message').textContent = '🤦‍♂️ Enter the number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
-
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
+
     document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
     if (score > 1) {
